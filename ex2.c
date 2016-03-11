62aaf0edc3ad0c8b27e7a4dc35713fc33c1cce59
JiniLee <gsdjini91@gmailc.com>
2016-03-11 14:13:45 +0900

    git

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+	int i;
+	printf("press enter 1 or 2\n");
+	scanf("number : %d",&i);
+	if(i==1){
+		printf("gsdjini91@gmail.com\n");
+	}else if(i==2){
+		printf("010-1234-5678\n");
+		}
+
+}
